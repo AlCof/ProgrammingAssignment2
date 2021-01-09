e92efeea171de592e9b2ee125f6fbebcd16f3099
AlCof <77095590+AlCof@users.noreply.github.com>
2021-01-09 16:29:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+setwd('D:/Coursera/RProgramming/Assignments')
+##
+## Setting input x as a matrix 
+## and setting the solved value as "n" as null
+## then changing every other reference to "mean" to "solve"
+## this function will create a special matrix which can cache its inverse
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  n <- NULL
+  set <- function(y) {
+    x <<- y
+    n <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) n <<- solve
+  getsolve <- function() n
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+##
+## similar step is done here, changing "mean" to "solve" and "m" to "n"
+## this part of the function would solve for 
+## the inverse of the matrix that was returned by the function above
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  n <- x$getsolve()
+  if(!is.null(n)) {
+    message("getting the inverse of the matrix")
+    return(n)
+  }
+  data <- x$get()
+  n <- solve(data, ...)
+  x$setsolve(n)
+  n
 }
